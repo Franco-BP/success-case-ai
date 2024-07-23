a1f40e6e3aa81075d2f160db5b26e02e983f167f
Franco Bascialla <francobascialla@gmail.com>
2024-07-23 18:55:07 -0300

    Added the /getdrive endpoint returning the formatted drive files

--- app/routes/chat_routes.py
+++ app/routes/chat_routes.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, request, current_app
 from ..services.vector_db_service import populate_vector_db
+from ..services.get_drive import *
 
 chat = Blueprint('chat', __name__)
 
@@ -9,3 +10,7 @@ def get_relational_cases():
     data = request.get_json()
     print(populate_vector_db())
     return {"a": populate_vector_db()}
+
+@chat.route('/getdrive', methods=['GET'])
+def get_drive_files():
+    return get_drives()
