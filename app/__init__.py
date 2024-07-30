4a656498f42856a4a2c08fb888bc2aa9e1911c7a
Matias Cabrera <matias.clavijo@koin.com.br>
2024-07-30 19:34:36 -0300

    v1.10

--- app/__init__.py
+++ app/__init__.py
@@ -1,22 +1,29 @@
+import atexit
+
 from flask import Flask
 from flask_cors import CORS
 from .config.config import Config
 
-from .src.clients.vector_db_client import VectorDB
 from .src.services.vector_db_service import populate_vector_db
-from .src.clients.model_client import ModelClient
-from .src.clients.google_drive_client import DriveClient
+from .src.jobs.update_success_cases import update_drive_success_cases
+from apscheduler.schedulers.background import BackgroundScheduler
 
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
 
+    scheduler = BackgroundScheduler()
     CORS(app, support_credentials=False)
     
     from .src.controllers.chat_controller import chat
 
     app.register_blueprint(chat, url_prefix='/chat')
+
+    scheduler.add_job(update_drive_success_cases, 'cron', hour=22, minute=0)
+    scheduler.start()
+
+    atexit.register(lambda: scheduler.shutdown())
 
     populate_vector_db("success_case")
 
